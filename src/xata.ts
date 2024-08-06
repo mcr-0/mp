d95ca6f63aa99d277a9c515171018ef5605ed7bc
Michal <m@michals-mbp.home>
2024-08-06 17:54:18 +0200

    bug fixed5

--- src/xata.ts
+++ src/xata.ts
@@ -7,6 +7,163 @@ import type {
 
 const tables = [
   {
+    name: "Account",
+    checkConstraints: {
+      Account_xata_id_length_xata_id: {
+        name: "Account_xata_id_length_xata_id",
+        columns: ["xata_id"],
+        definition: "CHECK ((length(xata_id) < 256))",
+      },
+    },
+    foreignKeys: {
+      Account_userId_fkey: {
+        name: "Account_userId_fkey",
+        columns: ["userId"],
+        referencedTable: "User",
+        referencedColumns: ["id"],
+        onDelete: "CASCADE",
+      },
+    },
+    primaryKey: ["id"],
+    uniqueConstraints: {
+      Account__pgroll_new_xata_id_key: {
+        name: "Account__pgroll_new_xata_id_key",
+        columns: ["xata_id"],
+      },
+    },
+    columns: [
+      {
+        name: "access_token",
+        type: "text",
+        notNull: false,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "expires_at",
+        type: "int",
+        notNull: false,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "id",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "id_token",
+        type: "text",
+        notNull: false,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "provider",
+        type: "text",
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "providerAccountId",
+        type: "text",
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "refresh_token",
+        type: "text",
+        notNull: false,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "scope",
+        type: "text",
+        notNull: false,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "session_state",
+        type: "text",
+        notNull: false,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "token_type",
+        type: "text",
+        notNull: false,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "type",
+        type: "text",
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "userId",
+        type: "link",
+        link: { table: "User" },
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "xata_createdat",
+        type: "datetime",
+        notNull: true,
+        unique: false,
+        defaultValue: "now()",
+        comment: "",
+      },
+      {
+        name: "xata_id",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: "('rec_'::text || (xata_private.xid())::text)",
+        comment: "",
+      },
+      {
+        name: "xata_updatedat",
+        type: "datetime",
+        notNull: true,
+        unique: false,
+        defaultValue: "now()",
+        comment: "",
+      },
+      {
+        name: "xata_version",
+        type: "int",
+        notNull: true,
+        unique: false,
+        defaultValue: "0",
+        comment: "",
+      },
+    ],
+  },
+  {
     name: "Postback",
     checkConstraints: {
       Postback_xata_id_length_xata_id: {
@@ -196,22 +353,151 @@ const tables = [
     ],
   },
   {
-    name: "_prisma_migrations",
-    checkConstraints: {
-      _prisma_migrations_xata_id_length_xata_id: {
-        name: "_prisma_migrations_xata_id_length_xata_id",
-        columns: ["xata_id"],
-        definition: "CHECK ((length(xata_id) < 256))",
+    name: "Session",
+    checkConstraints: {},
+    foreignKeys: {
+      Session_userId_fkey: {
+        name: "Session_userId_fkey",
+        columns: ["userId"],
+        referencedTable: "User",
+        referencedColumns: ["id"],
+        onDelete: "CASCADE",
       },
     },
+    primaryKey: ["id"],
+    uniqueConstraints: {},
+    columns: [
+      {
+        name: "expires",
+        type: "timestamp(3) without time zone",
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "id",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "sessionToken",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "userId",
+        type: "link",
+        link: { table: "User" },
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+    ],
+  },
+  {
+    name: "User",
+    checkConstraints: {},
     foreignKeys: {},
     primaryKey: ["id"],
-    uniqueConstraints: {
-      _prisma_migrations__pgroll_new_xata_id_key: {
-        name: "_prisma_migrations__pgroll_new_xata_id_key",
-        columns: ["xata_id"],
+    uniqueConstraints: {},
+    columns: [
+      {
+        name: "id",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: null,
+        comment: "",
       },
-    },
+      {
+        name: "username",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "xata_createdat",
+        type: "timestamp(6) with time zone",
+        notNull: true,
+        unique: false,
+        defaultValue: "CURRENT_TIMESTAMP",
+        comment: "",
+      },
+      {
+        name: "xata_id",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: "('rec_'::text || (xata_private.xid())::text)",
+        comment: "",
+      },
+      {
+        name: "xata_updatedat",
+        type: "timestamp(6) with time zone",
+        notNull: true,
+        unique: false,
+        defaultValue: "CURRENT_TIMESTAMP",
+        comment: "",
+      },
+      {
+        name: "xata_version",
+        type: "int",
+        notNull: true,
+        unique: false,
+        defaultValue: "0",
+        comment: "",
+      },
+    ],
+  },
+  {
+    name: "VerificationToken",
+    checkConstraints: {},
+    foreignKeys: {},
+    primaryKey: [],
+    uniqueConstraints: {},
+    columns: [
+      {
+        name: "expires",
+        type: "timestamp(3) without time zone",
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "identifier",
+        type: "text",
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "token",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: null,
+        comment: "",
+      },
+    ],
+  },
+  {
+    name: "_prisma_migrations",
+    checkConstraints: {},
+    foreignKeys: {},
+    primaryKey: ["id"],
+    uniqueConstraints: {},
     columns: [
       {
         name: "applied_steps_count",
@@ -277,99 +563,19 @@ const tables = [
         defaultValue: "now()",
         comment: "",
       },
-      {
-        name: "xata_createdat",
-        type: "datetime",
-        notNull: true,
-        unique: false,
-        defaultValue: "now()",
-        comment: "",
-      },
-      {
-        name: "xata_id",
-        type: "text",
-        notNull: true,
-        unique: true,
-        defaultValue: "('rec_'::text || (xata_private.xid())::text)",
-        comment: "",
-      },
-      {
-        name: "xata_updatedat",
-        type: "datetime",
-        notNull: true,
-        unique: false,
-        defaultValue: "now()",
-        comment: "",
-      },
-      {
-        name: "xata_version",
-        type: "int",
-        notNull: true,
-        unique: false,
-        defaultValue: "0",
-        comment: "",
-      },
-    ],
-  },
-  {
-    name: "user",
-    checkConstraints: {},
-    foreignKeys: {},
-    primaryKey: [],
-    uniqueConstraints: {},
-    columns: [
-      {
-        name: "xata_createdat",
-        type: "timestamp(6) with time zone",
-        notNull: true,
-        unique: false,
-        defaultValue: "CURRENT_TIMESTAMP",
-        comment: "",
-      },
-      {
-        name: "xata_id",
-        type: "text",
-        notNull: true,
-        unique: true,
-        defaultValue: "('rec_'::text || (xata_private.xid())::text)",
-        comment: "",
-      },
-      {
-        name: "xata_updatedat",
-        type: "timestamp(6) with time zone",
-        notNull: true,
-        unique: false,
-        defaultValue: "CURRENT_TIMESTAMP",
-        comment: "",
-      },
-      {
-        name: "xata_version",
-        type: "int",
-        notNull: true,
-        unique: false,
-        defaultValue: "0",
-        comment: "",
-      },
     ],
   },
 ] as const;
 
 export type SchemaTables = typeof tables;
 export type InferredTypes = SchemaInference<SchemaTables>;
-
-export type Postback = InferredTypes["Postback"];
-export type PostbackRecord = Postback & XataRecord;
-
-export type PrismaMigrations = InferredTypes["_prisma_migrations"];
-export type PrismaMigrationsRecord = PrismaMigrations & XataRecord;
-
-export type User = InferredTypes["user"];
-export type UserRecord = User & XataRecord;
-
 export type DatabaseSchema = {
+  Account: AccountRecord;
   Postback: PostbackRecord;
+  Session: SessionRecord;
+  User: UserRecord;
+  VerificationToken: VerificationTokenRecord;
   _prisma_migrations: PrismaMigrationsRecord;
-  user: UserRecord;
 };
 
 const DatabaseClient = buildClient();
@@ -387,7 +593,25 @@ export class XataClient extends DatabaseClient<DatabaseSchema> {
           "main",
         ...options,
       },
-      tables
+      tables,
     );
   }
 }
+
+export type Account = InferredTypes["Account"];
+export type AccountRecord = Account & XataRecord;
+
+export type Postback = InferredTypes["Postback"];
+export type PostbackRecord = Postback & XataRecord;
+
+export type Session = InferredTypes["Session"];
+export type SessionRecord = Session & XataRecord;
+
+export type User = InferredTypes["User"];
+export type UserRecord = User & XataRecord;
+
+export type VerificationToken = InferredTypes["VerificationToken"];
+export type VerificationTokenRecord = VerificationToken & XataRecord;
+
+export type PrismaMigrations = InferredTypes["_prisma_migrations"];
+export type PrismaMigrationsRecord = PrismaMigrations & XataRecord;
