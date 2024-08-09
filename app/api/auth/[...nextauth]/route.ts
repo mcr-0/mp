d0cd19b597c1d44b12a46a2b5d4432011e8381b4
Michal <m@michals-mbp.home>
2024-08-09 14:43:33 +0200

    supabase4

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -55,6 +55,5 @@ export const authOptions: AuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-export const handler = NextAuth(authOptions);
-
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
