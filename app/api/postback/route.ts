e829b537b866d3a41beed77f071bf84700c2d29f
M <mcr@fastmail.com>
2024-08-13 10:38:03 +0200

    New version6

--- app/api/postback/route.ts
+++ app/api/postback/route.ts
@@ -21,7 +21,15 @@ export async function GET(req: NextRequest) {
   const ran = searchParams.get("ran");
 
   // Prosta walidacja
-  if (!aff_sub2) {
+  if (
+    !aff_sub ||
+    !aff_sub2 ||
+    !offer_id ||
+    !affiliate_id ||
+    !session_ip ||
+    !payout ||
+    !offer_name
+  ) {
     return NextResponse.json(
       { error: "Missing required fields" },
       { status: 400 },
@@ -29,24 +37,24 @@ export async function GET(req: NextRequest) {
   }
 
   try {
-    const conversions = await prisma.event.create({
+    const postback = await prisma.postback.create({
       data: {
-        offer_id: parseInt(offer_id as string, 10) || undefined, // Konwersja na integer
-        offer_name: offer_name as string | undefined,
-        affiliate_id: affiliate_id as string | undefined,
-        source: source as string | undefined,
-        session_ip: session_ip as string | undefined,
-        payout: payout ? parseFloat(payout as string) : undefined,
-        aff_sub: aff_sub as string | undefined,
-        aff_sub2: aff_sub2 as string | undefined,
-        aff_sub3: aff_sub3 as string | undefined,
-        aff_sub4: aff_sub4 as string | undefined,
-        aff_sub5: aff_sub5 as string | undefined,
-        ran: ran as string | undefined,
+        offer_id: parseInt(offer_id as string, 10), // Explicitly converting to integer
+        offer_name: offer_name as string, // Ensure that offer_name is a string
+        affiliate_id: parseInt(affiliate_id as string, 10), // Explicitly converting to integer
+        source: source ?? undefined, // Can be undefined or a string
+        session_ip: session_ip as string, // Ensure session_ip is a string
+        payout: parseFloat(payout as string), // Explicitly converting to float
+        aff_sub: aff_sub as string, // Ensure aff_sub is a string
+        aff_sub2: aff_sub2 as string, // Ensure aff_sub2 is a string
+        aff_sub3: aff_sub3 as string, // Ensure aff_sub3 is a string
+        aff_sub4: aff_sub4 ?? undefined, // Can be undefined or a string
+        aff_sub5: aff_sub5 ?? undefined, // Can be undefined or a string
+        ran: ran ?? undefined, // Can be undefined or a string
       },
     });
 
-    return NextResponse.json(conversions);
+    return NextResponse.json(postback);
   } catch (error) {
     console.error(error);
     return NextResponse.json(
