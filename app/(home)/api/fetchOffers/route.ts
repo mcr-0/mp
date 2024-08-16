e5d4ca94f270fb9d43353bc61733acff5e0bd9ba
M <mcr@fastmail.com>
2024-08-16 22:11:45 +0200

    New version - stable9

--- app/(home)/api/fetchOffers/route.ts
+++ app/(home)/api/fetchOffers/route.ts
@@ -35,7 +35,7 @@ type Data = {
 
 export async function GET(req: NextRequest) {
   const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0];
-  //const ip = "23.83.131.27";
+  // const ip = "23.83.131.27";
   const session = await getServerSession(authOptions);
   const userId = session?.user?.id || "defaultUsername"; // Fallback to a default value if username is not available
 
