0b5ef851e966e0c3798fc78fa526bf7fc81008c4
M <mcr@fastmail.com>
2024-08-20 14:12:35 +0200

    test3

--- home backup/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "../authConfig"; // Import z pliku authConfig.ts
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
